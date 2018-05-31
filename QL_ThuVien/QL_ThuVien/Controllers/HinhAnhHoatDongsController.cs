8c2bd51c93cad9da453e0ce386ee9d8149a1b3ba
SinDT <sindt264@gmail.com>
2018-05-31 09:57:46 +0700

    Them va hien thi hinh anh

---
+++ QL_ThuVien/QL_ThuVien/Controllers/HinhAnhHoatDongsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QL_ThuVien.Models;
+
+namespace QL_ThuVien.Controllers
+{
+    public class HinhAnhHoatDongsController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: HinhAnhHoatDongs
+        public ActionResult Index()
+        {
+            var hinhAnhHoatDongs = db.HinhAnhHoatDongs.Include(h => h.HoatDong);
+            return View(hinhAnhHoatDongs.ToList());
+        }
+
+        // GET: HinhAnhHoatDongs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HinhAnhHoatDong hinhAnhHoatDong = db.HinhAnhHoatDongs.Find(id);
+            if (hinhAnhHoatDong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hinhAnhHoatDong);
+        }
+
+        // GET: HinhAnhHoatDongs/Create
+        public ActionResult Create()
+        {
+            ViewBag.HD_IDHoatDong = new SelectList(db.HoatDongs, "HD_IDHoatDong", "HD_ChuDe");
+            return View();
+        }
+
+        // POST: HinhAnhHoatDongs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "HA_IDHinhAnh,HD_IDHoatDong,HA_ChuThich,HA_NoiDung")] HinhAnhHoatDong hinhAnhHoatDong)
+        {
+            int datalength = (int)Request.Files["image"].InputStream.Length;
+            byte[] _byteArr = new byte[datalength];
+            Request.Files[0].InputStream.Read(_byteArr, 0, datalength);
+
+            if (ModelState.IsValid)
+            {
+                hinhAnhHoatDong.HA_NoiDung = _byteArr;
+                db.HinhAnhHoatDongs.Add(hinhAnhHoatDong);
+                db.SaveChanges();
+                //ModelState.AddModelError("", "Xong");
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.HD_IDHoatDong = new SelectList(db.HoatDongs, "HD_IDHoatDong", "HD_ChuDe", hinhAnhHoatDong.HD_IDHoatDong);
+            return View(hinhAnhHoatDong);
+        }
+
+        public ActionResult getImage(string id)
+        {
+            string strID = Request.QueryString["ID"];
+            int ID = -1;
+            if (int.TryParse(id, out ID))
+            {
+                //var ha = db.HinhAnhHoatDongs.Where(h => h.HD_IDHoatDong == ID).FirstOrDefault();
+                var ha = from p in db.HinhAnhHoatDongs where p.HD_IDHoatDong == ID select p;
+                foreach(var i in ha) { 
+                if (i == null || i.HA_NoiDung == null)
+                {
+                    ModelState.AddModelError("", "Loi");
+
+                }
+                //Response.ContentType = "image/jpeg";
+                Response.OutputStream.Write(i.HA_NoiDung.ToArray(), 0, i.HA_NoiDung.Length);
+                Response.Flush();}
+            }
+
+            return View();
+        }
+        // GET: HinhAnhHoatDongs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HinhAnhHoatDong hinhAnhHoatDong = db.HinhAnhHoatDongs.Find(id);
+            if (hinhAnhHoatDong == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.HD_IDHoatDong = new SelectList(db.HoatDongs, "HD_IDHoatDong", "HD_ChuDe", hinhAnhHoatDong.HD_IDHoatDong);
+            return View(hinhAnhHoatDong);
+        }
+
+        // POST: HinhAnhHoatDongs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "HA_IDHinhAnh,HD_IDHoatDong,HA_ChuThich,HA_NoiDung")] HinhAnhHoatDong hinhAnhHoatDong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hinhAnhHoatDong).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.HD_IDHoatDong = new SelectList(db.HoatDongs, "HD_IDHoatDong", "HD_ChuDe", hinhAnhHoatDong.HD_IDHoatDong);
+            return View(hinhAnhHoatDong);
+        }
+
+        // GET: HinhAnhHoatDongs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            HinhAnhHoatDong hinhAnhHoatDong = db.HinhAnhHoatDongs.Find(id);
+            db.HinhAnhHoatDongs.Remove(hinhAnhHoatDong);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            //HinhAnhHoatDong hinhAnhHoatDong = db.HinhAnhHoatDongs.Find(id);
+            //if (hinhAnhHoatDong == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            //return View(hinhAnhHoatDong);
+        }
+
+        // POST: HinhAnhHoatDongs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HinhAnhHoatDong hinhAnhHoatDong = db.HinhAnhHoatDongs.Find(id);
+            db.HinhAnhHoatDongs.Remove(hinhAnhHoatDong);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- QL_ThuVien/QL_ThuVien/Controllers/HoatDongController.cs
+++ QL_ThuVien/QL_ThuVien/Controllers/HoatDongController.cs
@@ -1,17 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using QL_ThuVien.Models;
 
 namespace QL_ThuVien.Controllers
 {
     public class HoatDongController : Controller
     {
+        private DataContext db = new DataContext();
+
         // GET: HoatDong
         public ActionResult Index()
         {
+            return View(db.HoatDongs.ToList());
+        }
+
+        // GET: HoatDong/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoatDong hoatDong = db.HoatDongs.Find(id);
+            if (hoatDong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hoatDong);
+        }
+
+        // GET: HoatDong/Create
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        // POST: HoatDong/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "HD_IDHoatDong,HD_ChuDe,HD_NoiDung,HD_NgayHoatDong,HD_NgayKetThuc")] HoatDong hoatDong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HoatDongs.Add(hoatDong);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(hoatDong);
+        }
+
+        // GET: HoatDong/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoatDong hoatDong = db.HoatDongs.Find(id);
+            if (hoatDong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hoatDong);
+        }
+
+        // POST: HoatDong/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "HD_IDHoatDong,HD_ChuDe,HD_NoiDung,HD_NgayHoatDong,HD_NgayKetThuc")] HoatDong hoatDong)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hoatDong).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hoatDong);
+        }
+
+        // GET: HoatDong/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoatDong hoatDong = db.HoatDongs.Find(id);
+            if (hoatDong == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hoatDong);
+        }
+
+        // POST: HoatDong/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HoatDong hoatDong = db.HoatDongs.Find(id);
+            db.HoatDongs.Remove(hoatDong);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
